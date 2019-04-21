3cf85afbe90e92fea654e627fa6d77f8c10d1172
santiw <santiago.weber91@gmail.com>
2019-04-21 17:47:57 -0300

    agregando la experiencia requerida al modelo

--- models/job_offer.rb
+++ models/job_offer.rb
@@ -3,7 +3,7 @@ class JobOffer
 
   attr_accessor :id, :user, :user_id, :title,
                 :location, :description, :is_active,
-                :updated_on, :created_on
+                :updated_on, :created_on, :required_experience
 
   validates :title, presence: true
 
@@ -16,6 +16,7 @@ class JobOffer
     @updated_on = data[:updated_on]
     @created_on = data[:created_on]
     @user_id = data[:user_id]
+    @required_experience = data[:required_experience]
   end
 
   def owner

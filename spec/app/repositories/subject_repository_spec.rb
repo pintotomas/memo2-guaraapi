8bfc0f72072c4ece83b096d065506fb42e31aa74
santiago bianco <santiago.bianco91@gmail.com>
2019-06-04 00:59:46 -0300

    fix pipeline

--- spec/app/repositories/subject_repository_spec.rb
+++ spec/app/repositories/subject_repository_spec.rb
@@ -1,8 +1,10 @@
+require 'byebug'
+
 describe SubjectRepository do
   let(:repository) { described_class.new }
 
-  let(:subject_one) do
-    subject_saved = Subject.new(name: 'Analisis 3', professor: 'Sirne', code: '6201')
+  let!(:subject_one) do
+    subject_saved = Subject.new(name: 'Analisis 3', professor: 'Sirne', code: '6201', quota: 9)
     repository.save(subject_saved)
     subject_saved
   end

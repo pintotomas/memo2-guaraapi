21f56ec35e694be7d540621e5b1eecc7f0c59a72
Tomas <tomasp834@gmail.com>
2019-06-08 01:26:23 -0300

    shouldnt be able to save an inscription if the subject was not saved

---
+++ spec/app/repositories/inscriptions_repository_spec.rb
@@ -0,0 +1,16 @@
+describe InscriptionsRepository do
+  let(:repository) { described_class.new }
+
+  let!(:inscription_one) do
+    inscription = Inscription.new(id: 1,
+                                  student_id: 'Rob123', subject_id: 245, status: 'inscripto')
+    inscription
+  end
+
+  describe 'find inscription by student and subject id' do
+    it 'should raise foreign key constraint violation if subject doesnt exist in database' do
+      expect { repository.save(inscription_one) }.to raise_error
+      Sequel::ForeignKeyConstraintViolation
+    end
+  end
+end

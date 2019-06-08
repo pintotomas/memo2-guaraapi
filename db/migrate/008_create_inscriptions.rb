edd17676db8d3d3d81e2a3f574ad297555b403de
Tomas <tomasp834@gmail.com>
2019-06-08 00:46:40 -0300

    Inscriptions database migrations

---
+++ db/migrate/008_create_inscriptions.rb
@@ -0,0 +1,15 @@
+Sequel.migration do
+  up do
+    create_table(:inscriptions) do
+      primary_key :id
+      foreign_key :subject_id, :subjects, on_delete: 'cascade'
+      String :student_id #es un string porque es el alias de telegram.
+      String :status
+      Integer :final_grade
+    end
+  end
+
+  down do
+    drop_table(:inscriptions)
+  end
+end

0eb653459b288eec3e86f8fa3e9dfb151a89405b
Tomas <tomasp834@gmail.com>
2019-06-08 14:30:02 -0300

    successfull inscription + test

---
+++ spec/app/controllers/inscriptions_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+require 'byebug'
+RSpec.describe '/students' do
+  #  pending "add some examples to #{__FILE__}" do
+  let(:subject_saved) do
+    Subject.new(name: 'Algebra 2', requires_proyector: true, requires_lab: false,
+                professor: 'F.I', id: 1001, quota: 9, type: 'coloquio')
+  end
+
+  before(:each) do
+    SubjectRepository.new.delete_all
+  end
+
+  it 'create valid inscription' do
+    SubjectRepository.new.save(subject_saved)
+    post '/alumnos', '{"nombre_completo":"Juan Perez","codigo_materia":"1001","username_alumno":"juanperez"}'
+    expect(last_response.status).to eq 201
+  end
+end

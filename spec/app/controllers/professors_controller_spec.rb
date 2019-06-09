6e4b32bc7a1703b6b9b4d42e9f3bebe5384064d1
santiago bianco <santiago.bianco91@gmail.com>
2019-06-09 18:15:18 -0300

    rest para la calificacion de alumnos

--- spec/app/controllers/professors_controller_spec.rb
+++ spec/app/controllers/professors_controller_spec.rb
@@ -1,13 +1,32 @@
 require 'spec_helper'
 
-# RSpec.describe '/professors' do
-#  pending "add some examples to #{__FILE__}" do
-#    before(:each) do
-#      get '/professors'
-#    end
-#
-#    it 'returns hello world' do
-#      expect(last_response.body).to eq 'Hello World'
-#    end
-#  end
-# end
+RSpec.describe '/professors' do
+  let!(:subject_saved) do
+    subject = Subject.new(name: 'Analisis 3',
+                          professor: 'Sirne', id: 6201, quota: 9, type: 'coloquio',
+                          requires_proyector: true, requires_lab: false)
+    SubjectRepository.new.save(subject)
+    subject
+  end
+
+  let!(:inscription_to_save) do
+    inscription = Inscription.new(id: 1, student_id: 'Rob123',
+                                  subject_id: subject_saved.id, status: 'inscripto')
+    inscription
+  end
+
+  after(:each) do
+    ScoresRepository.new.delete_all
+    SubjectRepository.new.delete_all
+  end
+
+  describe 'assign score to test' do
+    it 'assign score to test correctly' do
+      InscriptionsRepository.new.save(inscription_to_save)
+      params = { codigo_materia: subject_saved.id, notas: '1',
+                 username_alumno: inscription_to_save.student_id }
+      post '/calificar', params.to_json
+      expect(last_response.status).to eq 201
+    end
+  end
+end

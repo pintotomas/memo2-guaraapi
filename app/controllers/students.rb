066dfa36256d2c52d1acecc8ae33f9c6dafd401e
Tomas <tomasp834@gmail.com>
2019-06-12 12:48:34 -0300

    refactor controller

--- app/controllers/students.rb
+++ app/controllers/students.rb
@@ -19,13 +19,16 @@ GuaraApi::App.controllers :students do
   end
 
   get :miEstado, map: '/miEstado' do
-    # request_body = JSON.parse(request.body.read.gsub('\"', '"'))
     request_body = JSON.parse(request.params.keys[0].gsub('\"', '"'))
     alias_name = request_body['username_alumno']
     subject_id = request_body['codigo_materia']
-    inscribed = InscriptionsRepository.new.find_by_student_and_subject_id(alias_name, subject_id)
+    inscription = InscriptionsRepository.new.find_by_student_and_subject_id(alias_name, subject_id)
+    inscription_status = 'No inscripto'
+    if inscription
+      inscription_status = inscription.status
+    end
     status 201
-    body inscribed.status
+    body inscription_status
     return
   end
 

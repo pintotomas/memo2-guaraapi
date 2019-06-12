7585b3258f2d16a0e9277e47a46d813d4947fdd5
Tomas <tomasp834@gmail.com>
2019-06-12 16:29:21 -0300

    change NO INSCRIPTO to NO_INSCRIPTO

--- app/controllers/students.rb
+++ app/controllers/students.rb
@@ -22,9 +22,10 @@ GuaraApi::App.controllers :students do
     alias_name = request.params['usernameAlumno']
     subject_id = request.params['codigoMateria']
     inscription = InscriptionsRepository.new.find_by_student_and_subject_id(alias_name, subject_id)
-    inscription_status = 'NO INSCRIPTO'
-    final_grade = nil || inscription.final_grade
+    inscription_status = 'NO_INSCRIPTO'
+    final_grade = nil
     inscription_status = inscription.status if inscription
+    final_grade = inscription.final_grade if inscription
     status 200
     { 'estado' => inscription_status, 'nota_final' => final_grade }.to_json
   end

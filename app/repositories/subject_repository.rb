c9f7fbdf9eddb33f53bb082fe907accb357d7e6f
Tomas <tomasp834@gmail.com>
2019-06-17 14:16:27 -0300

    remove dead code

--- app/repositories/subject_repository.rb
+++ app/repositories/subject_repository.rb
@@ -2,13 +2,6 @@ class SubjectRepository < BaseRepository
   self.table_name = :subjects
   self.model_class = 'Subject'
 
-  # def save(a_record)
-  #  byebug
-  #  raise DuplicatedSubjectIdError if find_dataset_by_id(a_record.id).first
-
-  #  !insert(a_record).id.nil?
-  # end
-
   def find_by_name(name)
     subject_by_name = load_collection dataset.where(name: name)
     subject_by_name.first

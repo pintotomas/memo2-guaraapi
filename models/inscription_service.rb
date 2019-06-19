8fb67fe21305f07a8885148a55e619d0e92d8353
santiw <santiago.weber91@gmail.com>
2019-06-19 20:15:40 -0300

    agregando save en el service

--- models/inscription_service.rb
+++ models/inscription_service.rb
@@ -5,4 +5,10 @@ class InscriptionService
     @subject_repository = SubjectRepository.new
     @inscriptions_repository = InscriptionsRepository.new
   end
+
+  def save(inscription)
+    subject = @subject_repository.find(inscription.subject_id)
+    amount_in_course = @inscriptions_repository.inscriptions_in_course(subject.id)
+    @inscriptions_repository.save(inscription) if subject.quota > amount_in_course
+  end
 end
--- spec/app/models/inscription_service_spec.rb
+++ spec/app/models/inscription_service_spec.rb
@@ -1,8 +1,56 @@
 describe InscriptionService do
   subject(:service) { described_class.new }
 
+  after(:each) do
+    service.inscriptions_repository.delete_all
+    service.subject_repository.delete_all
+  end
+
+  let(:inscription3) do
+    inscription = Inscription.new(student_id: 'Rob1234', subject_id: 123, status: Inscription::INSCRIBED)
+    inscription
+  end
+  let(:inscription2) do
+    inscription = Inscription.new(student_id: 'Rob1231', subject_id: 123, status: Inscription::INSCRIBED)
+    inscription
+  end
+  let(:inscription1) do
+    inscription = Inscription.new(student_id: 'Rob1232', subject_id: 123, status: Inscription::INSCRIBED)
+    inscription
+  end
+  let(:subject_one) do
+    subject_one = Subject.new(name: 'Analisis 3',
+                              professor: 'Sirne', id: 123, quota: 2,
+                              type: 'coloquio', requires_proyector: true,
+                              requires_lab: false)
+    subject_one
+  end
+
   describe 'model' do
     it { is_expected.to respond_to(:subject_repository) }
     it { is_expected.to respond_to(:inscriptions_repository) }
+  end
+
+  describe 'save' do
+    it 'saves when inscriptions' do
+      service.subject_repository.save(subject_one)
+      service.save(inscription1)
+      expect(service.inscriptions_repository.inscriptions_in_course(subject_one.id)).to eq 1
+    end
+
+    it 'saves when inscriptions is less than quota' do
+      service.subject_repository.save(subject_one)
+      service.save(inscription1)
+      service.save(inscription2)
+      expect(service.inscriptions_repository.inscriptions_in_course(subject_one.id)).to eq 2
+    end
+
+    it 'doesnt save when inscriptions amount equals quota' do
+      service.subject_repository.save(subject_one)
+      service.save(inscription1)
+      service.save(inscription2)
+      service.save(inscription3)
+      expect(service.inscriptions_repository.inscriptions_in_course(subject_one.id)).to eq 2
+    end
   end
 end

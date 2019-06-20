9a46223ca74d74aaf6d924a00721c9f00041a563
santiago bianco <santiago.bianco91@gmail.com>
2019-06-19 21:26:42 -0300

    cambio la key de como se devuelve las materias en los rest para que pase el fitnesse

--- app/helpers/students_helper.rb
+++ app/helpers/students_helper.rb
@@ -8,7 +8,7 @@ module GuaraApi
         subjects.each do |subject|
           subject_response =
             { codigo: subject[:id],
-              materia: subject[:name],
+              nombre: subject[:name],
               docente: subject[:professor] }
 
           subjects_response.push(subject_response)
@@ -21,7 +21,7 @@ module GuaraApi
         subjects.each do |subject|
           subject_response =
             { codigo: subject[:subject_id],
-              materia: subject[:name],
+              nommbre: subject[:name],
               docente: subject[:professor] }
 
           subjects_response.push(subject_response)
